ae460f4930cf2eceaac72930ad4f235f71062aa7
Stephan Ruttloff <stephanruttloff@users.noreply.github.com>
2022-12-02 21:00:56 +0100

    Day 02 Part 2

--- Day02.cs
+++ Day02.cs
@@ -7,19 +7,25 @@
             var input = File.ReadAllLines("res/day02.txt");
             var outcomes = input
                 .Select(line => line.Split(" "))
-                .Select(split => (int)Decode(split[1]) + (int)GetOutcome(Decode(split[1]), Decode(split[0])));
+                .Select(split => new[] { ToHand(split[0]), GetHand(ToHand(split[0]), ToOutcome(split[1])) })
+                .Select(hands => (int)hands[1] + (int)GetOutcome(hands[1], hands[0]));
             var score = outcomes.Sum();
             Console.WriteLine($"Total score: {score}");
         }
 
-        private static Hand Decode(string encoded) => encoded switch
+        private static Hand ToHand(string encoded) => encoded switch
         {
             "A" => Hand.Rock,
-            "X" => Hand.Rock,
             "B" => Hand.Paper,
-            "Y" => Hand.Paper,
             "C" => Hand.Scissors,
-            "Z" => Hand.Scissors,
+            _ => throw new ArgumentOutOfRangeException(nameof(encoded))
+        };
+
+        private static Outcome ToOutcome(string encoded) => encoded switch
+        {
+            "X" => Outcome.Loose,
+            "Y" => Outcome.Draw,
+            "Z" => Outcome.Win,
             _ => throw new ArgumentOutOfRangeException(nameof(encoded))
         };
 
@@ -33,6 +39,17 @@
                     Hand.Scissors => opponent switch { Hand.Paper => Outcome.Win, _ => Outcome.Loose },
                     _ => throw new ArgumentOutOfRangeException(nameof(me)),
                 };
+
+        private static Hand GetHand(Hand opponent, Outcome outcome)
+            => outcome == Outcome.Draw
+            ? opponent
+            : opponent switch
+            {
+                Hand.Rock => outcome switch { Outcome.Win => Hand.Paper, _ => Hand.Scissors },
+                Hand.Paper => outcome switch { Outcome.Win => Hand.Scissors, _ => Hand.Rock },
+                Hand.Scissors => outcome switch { Outcome.Win => Hand.Rock, _ => Hand.Paper },
+                _ => throw new ArgumentOutOfRangeException(nameof(opponent)),
+            };
 
         private enum Hand
         {
