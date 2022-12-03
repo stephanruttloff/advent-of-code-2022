f58a85c2fff4eb1b42a02b53acf2cc841c32e271
Stephan Ruttloff <stephanruttloff@users.noreply.github.com>
2022-12-03 12:50:53 +0100

    Day 03 Part 2

--- Day03.cs
+++ Day03.cs
@@ -7,13 +7,21 @@ namespace advent_of_code_2022
         {
             const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-            var sum = File.ReadAllLines("res/day03.txt")
+            var part1 = File.ReadAllLines("res/day03.txt")
                 .Select(line => new[] {line[..(line.Length / 2)], line[(line.Length / 2)..] })
                 .Select(lineParts => lineParts[0].First(c => lineParts[1].Contains(c)))
                 .Select(commonElement => chars.IndexOf(commonElement) + 1)
                 .Sum();
 
-            Console.WriteLine($"Sum is {sum}");
+            Console.WriteLine($"Answer part 1: {part1}");
+
+            var part2 = File.ReadAllLines("res/day03.txt")
+                .Chunk(3)
+                .Select(group => group[0].First(c => group[1].Contains(c) && group[2].Contains(c)))
+                .Select(commonElement => chars.IndexOf(commonElement) + 1)
+                .Sum();
+
+            Console.WriteLine($"Answer part 2: {part2}");
         }
     }
 }
