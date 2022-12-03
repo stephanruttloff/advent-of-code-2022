a42f8883b2bd9d8147ace13e73b60bae1962a1d7
Stephan Ruttloff <stephanruttloff@users.noreply.github.com>
2022-12-03 12:43:40 +0100

    Day 03 Part 1

---
+++ Day03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace advent_of_code_2022
+{
+    public static class Day03
+    {
+        public static void PrintResult()
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var sum = File.ReadAllLines("res/day03.txt")
+                .Select(line => new[] {line[..(line.Length / 2)], line[(line.Length / 2)..] })
+                .Select(lineParts => lineParts[0].First(c => lineParts[1].Contains(c)))
+                .Select(commonElement => chars.IndexOf(commonElement) + 1)
+                .Sum();
+
+            Console.WriteLine($"Sum is {sum}");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -6,4 +6,7 @@ Day01.PrintResult();
 Console.WriteLine("Day02:");
 Day02.PrintResult();
 
+Console.WriteLine("Day03:");
+Day03.PrintResult();
+
 Console.ReadKey();
